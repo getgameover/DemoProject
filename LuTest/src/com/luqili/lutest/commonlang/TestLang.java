30fde9fc68aee2b16278ae5b90f29b257f44f132
luqili <luqili2006@126.com>
2016-06-30 14:20:46 +0800

    增加测试工程

---
+++ LuTest/src/com/luqili/lutest/commonlang/TestLang.java
@@ -0,0 +1,13 @@
+package com.luqili.lutest.commonlang;
+
+import org.apache.commons.lang.time.DateFormatUtils;
+
+public class TestLang {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Long t=null;
+		String time=DateFormatUtils.format(t, "yyyy-MM-dd");
+	}
+
+}
