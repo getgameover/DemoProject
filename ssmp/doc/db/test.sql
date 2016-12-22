b1e8781e443139dae16767af08ab33b60ad97883
getgameover <luqili2006@126.com>
2016-12-22 11:50:04 +0800

    修改示例文件

--- ssmp/doc/db/test.sql
+++ ssmp/doc/db/test.sql
@@ -1,31 +1,14 @@
+drop schema test;
 create schema test;
-create table test.a(
-    id serial primary key not null,
-    name varchar(64) not null
-);
-create table test.b(
-    id serial primary key not null,
-    name varchar(64) not null
-);
-insert into test.a(name)values('张三');
-insert into test.a(name)values('李四');
-insert into test.a(name)values('王五');
-
-insert into test.b(name)values('张三');
-
-select a.* as a,b.* from test.a as a  join test.b as b on a.name=b.name;
 drop table test.a;
 create table test.a(
     id serial primary key not null,
-    power decimal(64,0),
-    pp integer ,
-    name varchar(64)
+    name varchar(64) not null,
+    power bit(50) not null
 );
-insert into test.a(power,pp,name)values(111111111111111111111111,1111,"张三");
-insert into test.a(power,pp,name)values(111111111111111111111111,1111,"历史");
-insert into test.a(power,pp,name)values(111111111111111111111111,1111,"王五");
-insert into test.a(power,pp,name)values(222222222222222222222222,2222,"lao shi");
-insert into test.a(power,pp,name)values(333333333333333333333333,3333,"li shi");
+
+insert into test.a(name,power)values('张三','00000000000000000000000000000020000000011111111111');
+
 select pp&2=2 from test.a;
 select power >> 2 from test.a;
 select 1<<20,1<<52;
---
+++ ssmp/test/test/other/TestCt.java
@@ -0,0 +1,8 @@
+package test.other;
+
+import org.bouncycastle.jcajce.provider.symmetric.AES;
+
+public class TestCt {
+	public static void main(String[]args){
+	}
+}
